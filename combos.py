098b08b7aa3079c5075994bd4765da96aa4fc522
vicq20 <vicq20@kali>
2025-11-08 19:04:29 +0100

    Initial commit

---
+++ combos.py
@@ -0,0 +1,14 @@
+"""
+combos.py
+
+Generate every combinations of two words from a list of words.
+
+Usage:
+    python3 combos.py [options]
+    
+    combos.py can read a list of words from stdin or from a file. By default, outputs to stdout.
+    
+    The list of words must be made of words separated by newlines.
+    
+    
+    
